1f5b7dcb14c0d2e991a84b8a1ec122dc815c9359
Aakash Patel <akashpatelp83@gmail.com>
2022-10-31 02:42:34 +0530

    Add Day 2 String Methods Project
    Some basic string manipulation in-built methods

---
+++ 002_Day_2_String_Methods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization; // will be used for ToTitleCse conversion
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string MyString = "      This is a sentence for testing various string methods    ";
+
+        // Find Length
+        Console.WriteLine("Length : " + MyString.Length);
+        // Trim Methods
+        TrimMethods(MyString);
+
+        MyString = MyString.Trim();
+        // Case Conversion Methods
+        Conversions(MyString);
+
+        // Searching/finding Methods in a string
+        SearchString(MyString);
+
+    }
+
+    internal static void TrimMethods(string Sentence)
+    {
+        string FullTrim = Sentence.Trim();
+        string LeftPartTrim = Sentence.TrimStart();
+        string RightPartTrim = Sentence.TrimEnd();
+        Console.WriteLine("\nFull sentence: " + Sentence);
+        Console.WriteLine($"Full Trim: {FullTrim}, \nLeft part trimmed: {LeftPartTrim}, \nRight part trimmed: {RightPartTrim},");
+
+    }
+
+    public static void Conversions(string Sentence)
+    {
+        string UpperConvert = Sentence.ToUpper();
+        string LowerConvert = Sentence.ToLower();
+        // Non inbuilt for Title case; can make one using arrays.
+
+        Console.WriteLine("\nFull sentence: " + Sentence);
+        Console.WriteLine($"Upper: {UpperConvert}, \nLower: {LowerConvert},");
+    }
+
+    protected static void SearchString(string Sentence)
+    {
+        string ReplacedWord = Sentence.Replace("testing", "'the purpose of testing'");
+        bool CheckContains = Sentence.Contains("methods");
+        bool StartingWith = Sentence.StartsWith("This");
+        bool EndingWith = Sentence.EndsWith("testing");
+
+        Console.WriteLine("\nFull sentence: " + Sentence);
+        Console.WriteLine($"Replaced: {ReplacedWord}, \nContains 'methods'? {CheckContains}, \nStart 'This'? {StartingWith}, \nending 'testing'? {EndingWith},");
+
+    }
+}
--- day1-console-app01/Program.cs
+++ day1-console-app01/Program.cs
@@ -4,6 +4,24 @@
         Console.WriteLine("Hello, World!");
         Console.WriteLine("print my name !!");
         int result = Add(10, 13);
+        Console.WriteLine(Subtract(result, 5));
         Console.WriteLine("sum is " + result);
+        PrintName("Akash");
+    }
+
+    static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static void PrintName(string name)
+    {
+        Console.WriteLine($"You Name is: {name}");
+        SayHello();
+
+        void SayHello()
+        {
+            Console.WriteLine($"Hello {name}!");
+        }
     }
 }
