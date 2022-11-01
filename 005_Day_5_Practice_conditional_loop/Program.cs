07dd28cbd6d6cca7e2def0047921913296036642
Aakash Patel <akashpatelp83@gmail.com>
2022-11-02 02:17:25 +0530

    Add Day 5 file

---
+++ 005_Day_5_Practice_conditional_loop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_Day_4_Conditionals_Loops_practice
+{
+    class Program
+    {
+        public static void Main()
+        {
+            CheckAge("hey");
+            CheckAge("good bye");
+        }
+
+
+        public static void CheckAge(string args)
+        {
+            Console.WriteLine(args);
+            Console.Write("What is your age? ");
+            string age = Console.ReadLine();
+            int ageLimit = 18;
+            if (Int32.TryParse(age, out int ageInput))
+            {
+                //IF the input is greater than the limit, Write "Welcome"
+                if (ageInput >= ageLimit)
+                {
+                    Console.WriteLine("Welcome");
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    int underAge = ageLimit - ageInput;
+                    Console.WriteLine("Come back in " + underAge + " years, kid.");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+            }
+        }
+
+
+    }
+}
